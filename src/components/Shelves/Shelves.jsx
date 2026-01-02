5c3d1a6658bb040b1e953cba57a4fcff72db0125
MZ <mijgonzekria@gmail.com>
2026-01-02 17:16:56 -0500

    LOGIC FIX: used shelf id for url path instead of shelf name

--- src/components/Shelves/Shelves.jsx
+++ src/components/Shelves/Shelves.jsx
@@ -2,26 +2,29 @@ import React, { useState, useEffect } from "react";
 import "./Shelves.css";
 import { useParams } from "react-router-dom";
 import api from "../../api/axiosConfig";
+import { useGlobalContext } from "../../context";
 
 const Shelves = () => {
-  const { shelfName } = useParams();
+  const { shelfId } = useParams();
   const [booksInShelf, setBooksInShelf] = useState([]);
+  const { shelfOpen } = useGlobalContext();
 
   useEffect(() => {
     getBooks();
-  }, [shelfName]);
+  }, [shelfId]);
 
   async function getBooks() {
     try {
-      const bookResponse = await api.get(`/api/v1/shelf/getBooks/${shelfName}`);
+      const bookResponse = await api.get(`/api/v1/shelf/getBooks/${shelfId}`);
       setBooksInShelf(bookResponse.data);
     } catch (e) {
       console.error(`Error:`, e);
     }
   }
+  
   return (
     <div className="shelf-content">
-      <h2 className="shelf-title">{shelfName}</h2>
+      <h2 className="shelf-title">{shelfOpen.shelfName}</h2>
       <div className="book-grid">
         {booksInShelf.length > 0 ? (
           booksInShelf.map((book) => (
